c6f6bd9bad03be77988be3983bb1c40ab5706816
MISHA <vol.mykhailo@gmail.com>
2024-05-12 20:35:20 +0300

    rozetka11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotebooksComponent } from './notebooks/notebooks.component';
+import { ComputersComponent } from './computers/computers.component';
+import { MonitorsComponent } from './monitors/monitors.component';
+import { GamingComponent } from './gaming/gaming.component';
+import { TabletsComponent } from './tablets/tablets.component';
+import { CablesComponent } from './cables/cables.component';
+import { PartsComponent } from './parts/parts.component';
+import { NetworkComponent } from './network/network.component';
+import { HeadphonesComponent } from './headphones/headphones.component';
+import { KeyboardsComponent } from './keyboards/keyboards.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { OfficeComponent } from './office/office.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NotebooksComponent,ComputersComponent,MonitorsComponent,GamingComponent,TabletsComponent,CablesComponent,PartsComponent,NetworkComponent,HeadphonesComponent,KeyboardsComponent,ElectronicsComponent,OfficeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/keyboards/keyboards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keyboards',
+  standalone: true,
+  imports: [],
+  templateUrl: './keyboards.component.html',
+  styleUrl: './keyboards.component.css'
+})
+export class KeyboardsComponent {
+
+}
